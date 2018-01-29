5bd9339d817358d862b41f5de42f37751db01db1
曹先森 <cao_shixin@yahoo.com>
2018-01-29 11:47:23 +0800

    添加使用描述。

--- CSWaterLoadView/CSWaterLoadView/CSXWaterLoadView.h
+++ CSWaterLoadView/CSWaterLoadView/CSXWaterLoadView.h
@@ -9,9 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface CSXWaterLoadView : UIView
+//波动的速度，默认是6.0；
 @property (assign, nonatomic) CGFloat speed;
+//波动的高度，默认为6.0；
 @property (assign, nonatomic) CGFloat waveHeight;
+//波浪的数目，默认为2个；
 @property (assign, nonatomic) CGFloat waveNum;
+//波浪的高度，范围0～1，默认剧中0.5显示。
 @property (assign, nonatomic) CGFloat _h;
 
 - (instancetype)initWithFrame:(CGRect)frame title:(NSString *)title font:(UIFont *)font showLoadNote:(BOOL)show;
